acaed18b329a1240210dba52e9aecf09f9d203da
Janni Tselekoglou <jannitselekoglou@Air-von-Janni.fritz.box>
2022-02-20 01:31:49 +0100

    testing

---
+++ gradient_decent_test.py
@@ -0,0 +1,23 @@
+
+#input 10000 28x28 pixel image
+
+#28x28=784 input layer
+#increase bias
+#get better understanding: 
+# each neuron is connected to layer before that, with certain weights attachted to it and also a Bias number indicating activation
+#cost function takes output of NN and compares it to be a 1 or 0 therfore its just outputvalue - possiblevalue
+#  lower cost function means good values how good the NN acutally good , output 1 number 
+# imagin this as a function and try to find minimum from f(x)
+
+#using tangente helps decide in wich direction u should move your current possition to get the lowest value, --> aproches lokal minimum but how to find global minimumA
+
+#3d with 2 inputs --> find downhil diretion 
+
+#--> to find this minimum in 3d u can use the Gradient to find the direction of steepest increase
+# --> step into negative direction :Task find this vector 
+# Backpropagation
+#
+# Gradient descent 
+
+#Take the input vector 12h12h
+
